6ced2f58b7a2c44a64a6ca1a43a3795b79b50492
Scott Ross <scott.william.ross2@gmail.com>
2022-10-04 11:01:04 -0400

    added logo to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
 import styles from "../styles/components/Navbar.module.css";
 import Link from "next/link";
+import fbi from "../assets/images/fbi.webp"
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <h1 className={styles.logo}>
-                <Link href="/">FBI&apos;s Most Wanted</Link>
-            </h1>
+            <div className={styles.logo}>
+                <img src={fbi.src} />
+                <h1><Link href="/">FBI&apos;s Most Wanted</Link></h1>
+            </div>
             <ul className={styles.links}>
                 <li className={styles.first}><Link href="/art-crimes">Art Crimes</Link></li>
                 <li><Link href="/wanted">Most Wanted</Link></li>
